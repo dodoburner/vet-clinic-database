580bc9f0bce778b5925fdf4ece1b449b24ea102f
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-12 13:44:01 +0200

    Set primary key and update %mon query

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT date_of_birth FROM animals WHERE name IN ('Pikachu', 'Agumon');
 SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.5;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
  CREATE TABLE animals(
-    id INT, 
+    id INT GENERATED ALWAYS AS IDENTITY, 
     name TEXT,
     date_of_birth DATE, 
     escape_attempts INT, 
     neutered BOOLEAN, 
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );
