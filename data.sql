cbadc0eac15fa16e10c2fed5c321452097bd5802
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-12 12:34:23 +0200

    Setup database and add queries

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', '03-Jan-2020', 0, 10.23, TRUE);
+INSERT INTO animals VALUES (2, 'Gabumon', '15-Nov-2018', 2, 8, TRUE);
+INSERT INTO animals VALUES (3, 'Pikachu', '07-Jan-2021', 1, 15.04, FALSE);
+INSERT INTO animals VALUES (4, 'Devimon', '12-May-2017', 5, 11, TRUE);
